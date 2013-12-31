02d23f088bc54c62e8e1ad72975dd9ffefee9d18
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-12-31 16:26:08 -0500

    first ruby files

---
+++ ruby_formatter.rb
@@ -0,0 +1,25 @@
+=begin
+getting input from a user and displying it
+=end
+
+# this asks for the users first name and then
+# will capitalize the first letter of their input and
+# make it the only copy
+print "What's your first name?"
+first_name = gets.chomp
+first_name.capitalize!
+
+print "What's your last name?"
+last_name = gets.chomp
+last_name.capitalize!
+
+print "What city are you from?"
+city = gets.chomp
+city.capitalize!
+
+print "What state or province are you from?"
+state = gets.chomp
+state.upcase!
+
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
+
---
+++ ruby_intro.rb
@@ -0,0 +1,49 @@
+my_num = 25
+my_boolean = true
+my_string = "Ruby"
+my_num = 100
+
+add = 2 + 2
+subtract = 2 - 1
+mult = 2*2
+divide = 2 / 2
+exponentiation = 2**2
+modulo = 2%2
+
+print "Hello!"
+puts "Hello!"
+
+"Steven Paul Sanderson II".length
+"Steven Paul Sanderson II".reverse
+puts "steven paul sanderson II".upcase
+puts "STEVEN PAUL SANDERSON II".downcase
+# This is how you do comments, just like python
+
+=begin
+this is an example
+of a multi line
+comment. this would
+be accomplished in python
+by using the tripple apostrophe
+like so '''comments here'''
+=end
+
+name = "Steven Paul Sanderson II"
+# better way to write
+my_name = "Steven Paul Sanderson II"
+my_age = 32
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 /17
+
+# A little review
+name = "Steven Paul Sanderson II"
+name.downcase.reverse.upcase #use of multiple methods at the same time
+
+# this is a single line comment
+=begin
+this is a 
+multi line comment
+=end
+
