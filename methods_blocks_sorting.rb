032a8b5c9be1f406da696eda763930373022249b
Steven Paul Sanderson II <spsanderson@gmail.com>
2014-01-08 18:51:08 -0500

    finish up of methods blocks and sorting in ruby

--- methods_blocks_sorting.rb
+++ methods_blocks_sorting.rb
@@ -71,3 +71,62 @@ end
 
 1.times { puts "As am I!" }
 
+# method that capitalizes a word
+def capitalize(string) 
+  puts "#{string[0].upcase}#{string[1..-1]}"
+end
+
+capitalize("ryan") # prints "Ryan"
+capitalize("jane") # prints "Jane"
+
+# block that capitalizes each string in the array
+["ryan", "jane"].each {|string| puts "#{string[0].upcase}#{string[1..-1]}"} # prints "Ryan", then "Jane"
+
+# The block, {|i| puts i}, is passed the current
+# array item each time it is evaluated. This block
+# prints the item. 
+[1, 2, 3, 4, 5].each { |i| puts i }
+
+# This block prints the number 5 for each item.
+# (It chooses to ignore the passed item, which is allowed.)
+[1, 2, 3, 4, 5].each { |i| puts i * 5 }
+
+my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
+
+# Call the sort! method on my_array below.
+# my_array should then equal [1, 2, 3, 4, 5, 6, 7, 8, 9].
+my_array.sort!
+
+# library sorting code
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+
+# How might we sort! the books in alphabetical order? (Hint, hint)
+books.sort!
+
+book_1 = "A Wrinkle in Time"
+book_2 = "A Brief History of Time"
+book_1 <=> book_2
+book_2 <=> book_1
+
+# Descending order
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+# To sort our books in ascending order, in-place
+books.sort! { |firstBook, secondBook| firstBook <=> secondBook }
+# Sort your books in descending order, in-place below
+books.sort! { |firstBook, secondBook| secondBook <=> firstBook }
+
+def welcome(name)
+    puts "Welcome to Ruby #{name}!"
+end
+welcome("Steve")
+
+def welcome(name)
+    return "#{name}!"
+end
+welcome("steve")
+
+my_array = [1, 2, 3, 4, 5]
+my_array.each { |x| puts x**2 }
+
+fruits = ["orange", "apple", "banana", "pear", "grapes"]
+fruits.sort! { |firstFruit, secondFruit| secondFruit <=> firstFruit }
