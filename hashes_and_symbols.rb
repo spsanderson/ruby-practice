6129e4b4ddcf81beff9a4c36d7daf52f860f61cc
Steven Paul Sanderson II <spsanderson@gmail.com>
2014-01-19 10:30:14 -0500

    update to hashes and symbols

--- hashes_and_symbols.rb
+++ hashes_and_symbols.rb
@@ -22,3 +22,56 @@ creatures = { "weasels" => 0,
 }
 creatures["cake"]
 
+no_nil_hash = Hash.new("No nil here")
+
+menagerie = { :foxes => 2,
+  :giraffe => 1,
+  :weezards => 17,
+  :elves => 1,
+  :canaries => 4,
+  :ham => 1
+}
+
+books = { :Graph_Theory => 2,
+  :Math => 20,
+  :Cooking => 1
+}
+
+puts :books.object_id
+puts :menagerie.object_id
+
+puts "string".object_id
+puts "string".object_id
+puts :symbol.object_id
+puts :symbol.object_id
+
+# Write your code below!
+my_first_symbol = :my_symbol
+
+symbol_hash = {
+    :breakfast => "coffee",
+    :language => "Ruby",
+    :age => 32,
+    :married => "soon"
+}
+puts "#{symbol_hash}"
+
+strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+symbols = [] # or symbols = Array.new
+strings.each do |i|
+  symbols.push(i.to_sym)
+end
+
+symbols.each do |i|
+  puts "#{i}"
+end
+
+stings = ["HTML", "CSS", "JavaScripts", "Python", "Ruby"]
+symbols = Array.new
+stings.each do |i|
+  symbols.push(i.intern)
+end
+
+symbols.each do |i|
+  puts "#{i}"
+end
